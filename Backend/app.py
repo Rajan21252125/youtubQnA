348764ec634e3ad6471d0dc44a39ffbacd37ee17
AniketMishra23 <mishraaniket557@gmail.com>
2024-03-29 21:23:37 +0530

    Added search function

--- Backend/app.py
+++ Backend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from main import qna
+from Backend.gsearch import results
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -10,8 +11,6 @@ CORS(app, resources={"/api/*": {"origins": "*"}})
 def home():
     return "Welcome to the QnA API!"
 
-
-
 @app.route("/api", methods=["POST"])
 def questionAns():
     id = request.json["id"]
@@ -19,7 +18,11 @@ def questionAns():
     answer = qna(question,id)
     return jsonify({"answer": answer})  # Return answer as JSON
 
-# ... (include other functions for transcript retrieval and QnA)
+@app.route("/gsearch", method=["POST"])
+def gog_ser():
+    query = request.json["query"]
+    gs = results(query)
+    return gs
 
 if __name__ == "__main__":
     app.run(debug=True)  # Start the Flask development server
